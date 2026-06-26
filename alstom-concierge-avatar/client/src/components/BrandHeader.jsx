d77d3e107424aace0adc18e40181e2198846d119
SG-station <nael.sidali@alstomgroup.com>
2026-06-26 02:38:49 +0000

    Make Alstom logo clickable to alstom.com

--- alstom-concierge-avatar/client/src/components/BrandHeader.jsx
+++ alstom-concierge-avatar/client/src/components/BrandHeader.jsx
@@ -3,12 +3,19 @@ import React from "react";
 export default function BrandHeader() {
   return (
     <header className="absolute top-0 left-0 right-0 z-20 flex items-center justify-center px-8 pt-6 pb-4">
-      <img
-        src={import.meta.env.BASE_URL + "alstom.png"}
-        alt="Alstom"
-        className="h-16 w-auto select-none object-contain"
-        draggable={false}
-      />
+      <a
+        href="https://www.alstom.com/"
+        target="_blank"
+        rel="noreferrer noopener"
+        className="inline-flex"
+      >
+        <img
+          src={import.meta.env.BASE_URL + "alstom.png"}
+          alt="Alstom"
+          className="h-16 w-auto select-none object-contain cursor-pointer"
+          draggable={false}
+        />
+      </a>
     </header>
   );
 }
